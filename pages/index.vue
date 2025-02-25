e411ac61304962961cf02605f1703b42528da08d
Muh Syahendra Anindyantoro <57746184+hendras1722@users.noreply.github.com>
2025-02-25 21:05:45 +0700

    feat: add mapStore utility and update sidebar store functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
-    oioioi
+    <UButton variant="solid" @click="getData">update Data </UButton> {{ openSidebarStore }}
   </div>
 </template>
 
 <script setup lang="ts">
-
+const { openSidebarStore, getData } = mapStore(useSideBar, ['openSidebarStore', 'getData']);
 </script>
-
-<style scoped></style>
